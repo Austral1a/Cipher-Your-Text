f583d08ace4e9fef68411aec7d13aa09370a0a25
Dan Korzh <lienglex@gmail.com>
2020-10-11 13:19:29 +0300

    add: alphabetRU constant, constructor and accessor methods

--- src/cipherAlgorithm/roti13.ts
+++ src/cipherAlgorithm/roti13.ts
@@ -7,3 +7,54 @@ interface ICipher {
     encode(): string;
     decode(): string;
 }
+
+const Cipher: CipherConstructor = class Cipher implements ICipher {
+    readonly alphabetRU = {
+        'а': 'м',
+        'б': 'н',
+        'в': 'о',
+        'г': 'п',
+        'д': 'р',
+        'е': 'с',
+        'ё': 'т',
+        'ж': 'у',
+        'з': 'ф',
+        'и': 'х',
+        'й': 'ц',
+        'к': 'ч',
+        'л': 'ш',
+        'м': 'щ',
+        'н': 'ъ',
+        'о': 'ы',
+        'п': 'ь',
+        'р': 'э',
+        'с': 'ю',
+        'т': 'я',
+        'у': 'а',
+        'ф': 'б',
+        'х': 'в',
+        'ц': 'г',
+        'ч': 'д',
+        'ш': 'е',
+        'щ': 'ё',
+        'ъ': 'ж',
+        'ы': 'з',
+        'ь': 'и',
+        'э': 'й',
+        'ю': 'к',
+        'я': 'л'
+    }
+    constructor(private _input: string, private _output: string) {}
+
+    private get input(): string {
+        return this._input
+    }
+
+    private get output(): string {
+        return this._output
+    }
+    private set output(newOutput: string) {
+        this._output = newOutput
+    }
+
+}
